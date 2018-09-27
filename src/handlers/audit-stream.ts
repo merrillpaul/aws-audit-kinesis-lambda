5d59c6ba4e097c1efa6e0cb364f3ecea8cd6ea62
merrillpaul <merrill.paul@pearson.com>
2018-09-27 18:36:19 -0500

    WIP

--- src/handlers/audit-stream.ts
+++ src/handlers/audit-stream.ts
@@ -2,8 +2,8 @@ import { Handler, Callback } from 'aws-lambda';
 
 export const auditLog: Handler = async (event: any, context: any, callback: Callback) => {
     event.Records.forEach((record: any) => {
-        const payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
-        console.log("Received a Kinesis event: " + payload);
+        const payload = new Buffer(record.kinesis.data, 'base64').toString('utf-8');
+        console.log("Received a Kinesis event: ", JSON.parse(payload));
     });
     callback(null, `Successfully processed ${event.Records.length} event.`);
 };
