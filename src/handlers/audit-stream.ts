68c415fe6328bbc7ba9f9f17971684997f561dfd
merrillpaul <merrill.paul@pearson.com>
2018-09-28 16:23:15 -0500

    Added spec for audit stream to call elasticlient

--- src/handlers/audit-stream.ts
+++ src/handlers/audit-stream.ts
@@ -64,7 +64,7 @@ export const auditLog: Handler = (event: any, context: any, callback: Callback) 
     event.Records.forEach((record: any) => {
         const payload = new Buffer(record.kinesis.data, 'base64').toString('utf-8');
         const events: AssessAuditEvent[] = JSON.parse(payload);
-        console.log("Received a Kinesis event: ", events);
+        // console.log("Received a Kinesis event: ", events);
         esClient.processAssessLogs(events);
     });
     callback(null, `Successfully processed ${event.Records.length} event.`);
